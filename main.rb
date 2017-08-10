cd2f915480c67f958b42df71009d9f839e04a73c
koutwiring <koutwiring@gmail.com>
2017-08-10 13:47:01 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -4,6 +4,6 @@ require "./get_filelist"
 
 begin
   file_list = get_file_hash(".")
-  file_list = get_file_detail(file_list, "title")
+  file_list = get_file_detail(file_list, "h1")
   print_html_li(file_list)
 end
